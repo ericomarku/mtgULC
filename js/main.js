08f07d18151c3b654111634ad4186c31a472545d
ericomarku <erik1850@gmail.com>
2022-10-17 06:51:32 +0200

    lock orientation

--- js/main.js
+++ js/main.js
@@ -150,32 +150,9 @@ $(document).ready(function(){
     $(document).toggleFullScreen(true)
   });
 
-  // // check native support
-	// $('#support').text($.fullscreen.isNativelySupported() ? 'supports' : 'doesn\'t support');
-	// // open in fullscreen
-	// $('#fullscreen .requestfullscreen').click(function() {
-	// 	$('#fullscreen').fullscreen();
-	// 	return false;
-	// });
-	// // exit fullscreen
-	// $('#fullscreen .exitfullscreen').click(function() {
-	// 	$.fullscreen.exit();
-	// 	return false;
-	// });
-	// // document's event
-	// $(document).bind('fscreenchange', function(e, state, elem) {
-	// 	// if we currently in fullscreen mode
-	// 	if ($.fullscreen.isFullScreen()) {
-	// 		$('#fullscreen .requestfullscreen').hide();
-	// 		$('#fullscreen .exitfullscreen').show();
-	// 	} else {
-	// 		$('#fullscreen .requestfullscreen').show();
-	// 		$('#fullscreen .exitfullscreen').hide();
-	// 	}
-	// 	$('#state').text($.fullscreen.isFullScreen() ? '' : 'not');
-	// });
-
   function setup() {
+
+    screen.orientation.lock("landscape")
 
     playerGrid.addClass('numP' + gameState[6][0]).css('opacity', 1);
     let playerCards = $('.playerCard');
